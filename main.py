96f566f2561a03eec897a0d108e67dd6f84b0b5d
Jack Bendtsen <jackdbendtsen@gmail.com>
2019-11-20 11:36:10 +1100

    Added Save and Reply buttons, enabled editing in unfiltered mode

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, filedialog
 
 import threading
 import os
@@ -33,11 +33,34 @@ def str2ba(string) :
 
 	return buf
 
+def ba2str(buf) :
+	length = len(buf) * 3
+	text = [' '] * length
+
+	idx = 0
+	for b in buf:
+		d = b >> 4
+		if d >= 0xa:
+			text[idx] = chr(87 + d)
+		else:
+			text[idx] = chr(48 + d)
+
+		d = b & 0xf
+		if d >= 0xa:
+			text[idx+1] = chr(87 + d)
+		else:
+			text[idx+1] = chr(48 + d)
+
+		idx += 3
+
+	return "".join(text)
+
 class Tab:
 	def __init__(self, gui, idx) :
 		self.main = gui
 		self.name = "Tab {0}".format(idx+1)
 		self.data = bytearray([])
+		self.filter = ""
 		self.use_filter = False
 
 	def append_byte(self, byte) :
@@ -45,47 +68,45 @@ class Tab:
 		if not self.use_filter:
 			self.main.recv.insert(tk.END, "{0:02x} ".format(byte))
 
+	def maybe_edit(self) :
+		if not self.use_filter :
+			self.data = str2ba(self.main.recv.get("1.0", "end"))
+
 	def update(self) :
+		self.main.recv.configure(state="normal")
 		self.main.recv.delete("1.0", tk.END)
 
+		text = ""
+		label = ""
 		if self.use_filter:
-			cmd = self.main.filter_txt.get()
+			cmd = self.filter
 
 			# if a filter was given, use it as a shell command and provide our data to stdin
 			if len(cmd) > 0:
-				proc = subprocess.Popen(args=shlex.split(cmd), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-				proc.stdin.write(self.data)
-				proc.stdin.close()
-				text = proc.stdout.read()
+				try:
+					proc = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+					proc.stdin.write(self.data)
+					proc.stdin.close()
+					text = proc.stdout.read().decode('cp437')
+					text += "\n" + proc.stderr.read().decode('cp437')
+
+				except (FileNotFoundError, OSError) as e:
+					text = "Filter Error: {0}".format(e)
 			# otherwise treat our data as straight-up ASCII
 			else:
 				text = self.data.decode('cp437')
+
+			label = "Receiving (Filtered Mode)"
 		else:
-			text = self.raw_text()
+			text = ba2str(self.data)
+			label = "Receiving (Edit Mode)"
 
 		self.main.recv.insert(tk.END, text)
 
-	def raw_text(self) :
-		length = len(self.data) * 3
-		text = [' '] * length
+		editing = "disabled" if self.use_filter else "normal"
+		self.main.recv.configure(state=editing)
 
-		idx = 0
-		for b in self.data:
-			d = b >> 4
-			if d >= 0xa:
-				text[idx] = chr(87 + d)
-			else:
-				text[idx] = chr(48 + d)
-
-			d = b & 0xf
-			if d >= 0xa:
-				text[idx+1] = chr(87 + d)
-			else:
-				text[idx+1] = chr(48 + d)
-
-			idx += 3
-
-		return "".join(text)
+		self.main.recv_lbl.config(text=label)
 
 class Breakfast:
 	def __init__(self, master, iface) :
@@ -113,30 +134,40 @@ class Breakfast:
 		self.filter_lbl = tk.Label(text="Filter")
 		self.filter_lbl.grid(row=1, columnspan=4, sticky="w")
 
-		self.use_filter = tk.IntVar()
+		self.filter_cb_var = tk.IntVar()
 
-		self.filter_cb = tk.Checkbutton(self.master, padx=10, variable=self.use_filter, command=self.toggle_filter)
+		self.filter_cb = tk.Checkbutton(self.master, padx=10, variable=self.filter_cb_var, command=self.toggle_filter)
 		self.filter_cb.grid(row=2)
 
-		self.filter_txt = tk.Entry(self.master, width=200)
+		self.filter_txt_var = tk.StringVar()
+		self.filter_txt_var.trace("w", self.update_filter)
+
+		self.filter_txt = tk.Entry(self.master, width=200, textvariable=self.filter_txt_var)
 		self.filter_txt.bind("<Return>", (lambda event: self.refresh()))
 		self.filter_txt.grid(row=2, column=1, columnspan=3)
 
-		self.recv_lbl = tk.Label(self.master, text="Receiving")
+		self.recv_lbl = tk.Label(self.master, text="Receiving (Edit Mode)")
 		self.recv_lbl.grid(row=3, columnspan=4, sticky="w")
 
 		self.recv = tk.Text(self.master, width=200, height=100)
+		self.recv.bind("<FocusOut>", lambda event: self.tabs[self.cur_tab].maybe_edit())
 		self.recv.grid(row=4, columnspan=4)
 
+		self.save_btn = tk.Button(self.master, text="Save", command=self.save)
+		self.save_btn.grid(row=5, column=2)
+
+		self.reply_btn = tk.Button(self.master, text="Reply", command=self.reply)
+		self.reply_btn.grid(row=5, column=3)
+
 		self.prompt_lbl = tk.Label(self.master, text="Command")
-		self.prompt_lbl.grid(row=5, columnspan=4, sticky="w")
+		self.prompt_lbl.grid(row=6, columnspan=4, sticky="w")
 
 		self.prompt = tk.Entry(self.master, width=200)
 		self.prompt.bind("<Key>", self.send_key_down)
-		self.prompt.grid(row=6, columnspan=3)
+		self.prompt.grid(row=7, columnspan=3)
 
 		self.send_btn = tk.Button(self.master, text="Send", command=self.send)
-		self.send_btn.grid(row=6, column=3)
+		self.send_btn.grid(row=7, column=3)
 
 		self.master.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -145,16 +176,26 @@ class Breakfast:
 		self.comms = comms.Comms(self, iface)
 		self.comms.start()
 
-	def send_key_down(self, e) :
-		if e.keysym == "Return" and (e.state & 1) == 0:
-			self.send()
-
 	def close(self) :
 		if self.comms != None:
 			self.comms.close()
 			self.comms.join()
 
 		self.master.destroy()
+
+	def send_key_down(self, e) :
+		if e.keysym == "Return" and (e.state & 1) == 0:
+			self.send()
+
+	def update_filter(self, *args) :
+		s = self.filter_txt_var.get()
+		self.tabs[self.cur_tab].filter = s
+
+	def toggle_filter(self) :
+		tab = self.tabs[self.cur_tab]
+		tab.maybe_edit()
+		tab.use_filter = self.filter_cb_var.get() == 1
+		tab.update()
 
 	def refresh_tab_list(self) :
 		if not self.init:
@@ -180,7 +221,6 @@ class Breakfast:
 		self.cur_tab = idx
 		self.master.title("Breakfast - " + self.tabs[idx].name)
 		self.refresh()
-		self.refresh_tab_list()
 
 	def add_tab(self) :
 		tab_id = self.n_created_tabs
@@ -210,18 +250,43 @@ class Breakfast:
 		if self.init:
 			self.tab_strs.set(self.tabs[self.cur_tab].name)
 
-
-	def toggle_filter(self) :
-		tab = self.tabs[self.cur_tab]
-		tab.use_filter = self.use_filter.get() == 1
-		tab.update()
-
 	def refresh(self) :
-		if self.init:
-			self.tabs[self.cur_tab].update()
+		if not self.init:
+			return
+
+		t = self.tabs[self.cur_tab]
+		t.update()
+
+		self.filter_txt_var.set(t.filter)
+		self.filter_cb_var.set(1 if t.use_filter else 0)
+
+		self.refresh_tab_list()
 
 	def append_byte(self, byte) :
 		self.tabs[self.cur_tab].append_byte(byte)
+
+	def save(self) :
+		t = self.tabs[self.cur_tab]
+		t.maybe_edit()
+		fmode = "w" if t.use_filter else "wb"
+
+		f = filedialog.asksaveasfile(mode=fmode)
+		if f is None:
+			return
+
+		if t.use_filter:
+			#self.recv.configure(state="normal")
+			f.write(self.recv.get("1.0", "end"))
+			#self.recv.configure(state="disabled")
+		else:
+			f.write(t.data)
+
+		f.close()
+
+	def reply(self) :
+		t = self.tabs[self.cur_tab]
+		t.maybe_edit()
+		self.comms.send(t.data)
 
 	def send(self) :
 		buf = str2ba(self.prompt.get())
